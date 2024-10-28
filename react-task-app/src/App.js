dc466cb66a4a4700115f88cec85a7be0232e0629
Patcharaporn Chanapal <107676580+BeauUS27@users.noreply.github.com>
2024-10-28 23:42:21 +0700

    State

--- react-task-app/src/App.js
+++ react-task-app/src/App.js
@@ -2,14 +2,24 @@ import "./App.css";
 import Header from "./components/Header";
 import AddForm from "./components/AddForm";
 import Item from "./components/Item";
+import { useState } from "react";
 
 function App() {
+  const [tasks,setTasks] = useState([
+    {id:1, title:"study react"},
+    {id:2, title:"study full stack"}
+  ])
   return (
     <div className="App">
       <Header />
       <div className="container">
         <AddForm />
-        <Item />
+        <section>
+          {tasks.map((data)=>(
+            <Item key={data.id} data={data}/>
+          ))}
+        </section>
+        
       </div>
     </div>
   );
