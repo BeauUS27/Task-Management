f37cc6a97da4b3d28cf22b508e79a6f44b3f4dea
Patcharaporn Chanapal <107676580+BeauUS27@users.noreply.github.com>
2024-11-06 13:51:27 +0700

    Icon

--- react-task-app/src/components/Item.js
+++ react-task-app/src/components/Item.js
@@ -1,12 +1,14 @@
 import "./Item.css"
+import { BsTrash } from "react-icons/bs";
+import { FaRegEdit } from "react-icons/fa";
 export default function Item(props){
     const {data, deleteTask, editTask} = props;
     return(
         <div className="list-item">
         <p className="title">{data.title}</p>
         <div className="button-container">
-            <button className="btn" onClick={()=>deleteTask(data.id)}>ลบ</button>
-            <button className="btn" onClick={()=>editTask(data.id)}>แก้ไข</button>
+            <BsTrash className="btn" onClick={()=>deleteTask(data.id)} />
+            <FaRegEdit className="btn" onClick={()=>editTask(data.id)} />
         </div>
         </div>
     );
